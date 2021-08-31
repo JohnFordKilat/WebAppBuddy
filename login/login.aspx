57b2bb80e7a7e22a0c2e1264a195d353de5ff3e4
JohnFordKilat <78631202+JohnFordKilat@users.noreply.github.com>
2021-08-31 17:52:04 +0800

    initial

---
+++ login/login.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="login.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 560px">
+            Username
+            <asp:TextBox ID="usernameTextbox" runat="server"></asp:TextBox>
+            <br />
+            Password
+            <asp:TextBox ID="passwordTextbox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="loginButton" runat="server" OnClick="loginButton_Click" Text="Login" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ login/login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace login
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            Boolean found = false;
+            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\jfkta\Pictures\Database\login.mdb";
+            OleDbConnection thisConnection = new OleDbConnection(connectionString);
+            thisConnection.Open();
+            OleDbCommand thisCommand = thisConnection.CreateCommand();
+
+            thisCommand.CommandText = "SELECT * FROM EMPLOYEEFILE";
+            OleDbDataReader thisReader = thisCommand.ExecuteReader();
+
+            while(thisReader.Read())
+            {
+                if(thisReader["username"].ToString().ToLower() == usernameTextbox.Text.ToLower())
+                {
+                    Response.Write("<script>alert('USER IS IN DATABASE')</script>");
+                    Response.Redirect("register.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('USER IS NOT IN DATABASE')</script>");
+                }
+            }
+
+            
+        }
+    }
+}
